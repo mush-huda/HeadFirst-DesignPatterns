992133a9c9042e49ffc270ec71970ab3bf87ebd8
Mushfiqul Huda <mushfiqul.onee@gmail.com>
2020-03-07 18:15:16 +0100

    The Singleton Pattern

---
+++ 5.Singleton/src/Singleton.java
@@ -0,0 +1,16 @@
+
+public class Singleton {
+	
+	private static Singleton uniqueInstance;
+	
+	private Singleton(){
+		
+	}
+
+	public static Singleton getUniqueInstance() {
+		if(uniqueInstance == null) {
+			uniqueInstance = new Singleton();
+		}
+		return uniqueInstance;
+	}
+}
